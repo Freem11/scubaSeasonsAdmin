150422044551151c036e8574846e1ddba25f85a5
Freem11 <freem1985@gmail.com>
2024-06-23 23:32:36 -0600

    email notifications created

--- src/apicalls/supabaseCalls/partnerRequestSupabaseCalls.ts
+++ src/apicalls/supabaseCalls/partnerRequestSupabaseCalls.ts
@@ -7,7 +7,7 @@ export const partnerRequests = async () => {
   .is('validated', null)
 
   if (error) {
-    console.log("couldn't do it,", error);
+    console.log("couldn't do it 1,", error);
     return [];
   }
 
@@ -23,7 +23,37 @@ export const updatePartnerRequestByUserId= async (userID : string, decision : bo
   .eq('userId', userID)
 
   if (error) {
-    console.log("couldn't do it,", error);
+    console.log("couldn't do it 2,", error);
     return [];
+  }
+};
+
+export const updateProfileByUserId= async (userID : string, decision : boolean) => {
+  const { error } = await supabase
+  .from('UserProfiles')
+  .update({ 'partnerAccount' : decision })
+  .eq('UserID', userID)
+
+  if (error) {
+    console.log("couldn't do it 3,", error);
+    return [];
+  }
+};
+
+export const insertNewShop = async (businessName: string, lat: number, lng: number) => {
+  const { data, error } = await supabase.from("shops").insert([
+    {
+      orgName: businessName,
+      lat: lat,
+      lng: lng,
+    },
+  ]);
+
+  if (error) {
+    console.log("couldn't do it 4,", error);
+  }
+
+  if (data) {
+    console.log(data);
   }
 };
