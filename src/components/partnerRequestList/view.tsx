b45251f754f262d77fdcbae1c72e828388d0c0d5
Gabriel Alves <alvesgabriel1994@gmail.com>
2025-02-04 09:41:31 -0700

    web-111 remove setup map
    Will redo it properly when working on web-112

--- src/components/partnerRequestList/view.tsx
+++ src/components/partnerRequestList/view.tsx
@@ -1,49 +1,23 @@
-import { useContext } from "react";
-import { SelectedPartnerRequestContext } from "../../contexts/partnerRequestEvals/selectedPartnerRequestContext"
 import style from './styles.module.scss';
-import { MapContext } from "../googleMap/mapContext";
-import { SelectedSeaLifeContext } from "../../contexts/seaLifeEvals/selectedSeaLifePhotoContext";
-import { SitesArrayContext } from "../../contexts/sitesArrayContext";
 import { PartnerRequest } from '../../entities/partnerRequest';
 
 type PartnerRequestListProps = {
-    partnerRequestsList: PartnerRequest[] | null
-  };
+  partnerRequestsList: PartnerRequest[] | null;
+};
 
 export default function PartnerRequestListView(props: PartnerRequestListProps) {
-    const { setInitialPoint, mapRef } = useContext(MapContext);
-    const { setSelectedPartnerRequest } = useContext(SelectedPartnerRequestContext)
-    const { setSelectedSeaLife } = useContext(SelectedSeaLifeContext)
-    const { sitesArray, setSitesArray } = useContext(SitesArrayContext);
-
-    const setupMap = (record: PartnerRequest) => {
-        if(sitesArray.find(item => item.id === record.id)){
-            // const index = sitesArray.findIndex(item => item.id === record.id)
-            setSitesArray([])
-            setSelectedPartnerRequest(null)
-            // sitesArray.splice(index, 1);  
-        } else {
-            setSitesArray([{id: record.id, lat: record.latitude, lng: record.longitude, name: record.businessName}])
-            setSelectedPartnerRequest(record)
-        }
-
-        setSelectedSeaLife(null)
-        setInitialPoint([record?.latitude, record?.longitude]);
- 
-        mapRef?.panTo({ lat: record?.latitude, lng: record?.longitude });
-    };
-
-return (
+  return (
     <div className="mt-4 flex-column">
-    {props.partnerRequestsList && props.partnerRequestsList.map((record: PartnerRequest) => {
-        return (
-        <div className={style.cardMain} key={record.id} onClick={() => setupMap(record)}>
-            <div className='py-2'>
-                <div>{record.businessName}</div> 
+      {props.partnerRequestsList &&
+        props.partnerRequestsList.map((record: PartnerRequest) => {
+          return (
+            <div className={style.cardMain} key={record.id} onClick={() => {}}>
+              <div className="py-2">
+                <div>{record.businessName}</div>
+              </div>
             </div>
-        </div>
-    )
-    })}
-</div>
-)
+          );
+        })}
+    </div>
+  );
 }
