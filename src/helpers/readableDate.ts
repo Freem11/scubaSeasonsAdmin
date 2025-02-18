cc528b8586cb6b64584b6d1a268eff6ac0beab45
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-02-17 17:33:54 -0700

    staging: corrected readble date formatter for timezone issue

--- src/helpers/readableDate.ts
+++ src/helpers/readableDate.ts
@@ -1,8 +1,10 @@
 export default function readableDate(date: string) {
+  console.log("gets", date)
   const formattedDate = new Intl.DateTimeFormat('en-US', {
     month: 'long',
     day:   'numeric',
     year:  'numeric',
-  }).format(new Date(date));
+  }).format(new Date(`${date}T00:00:00`));
+  console.log("sends", formattedDate)
   return formattedDate;
 }
